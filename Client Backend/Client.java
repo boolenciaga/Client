6e20a850c2fa4137877163792429250321511cb0
majinmilad <49212431+majinmilad@users.noreply.github.com>
2020-03-20 22:49:43 -0700

    make Client class to handle socket communication
    also changed folder name to "UI"

--- Client Backend/Client.java
+++ Client Backend/Client.java
@@ -9,24 +9,42 @@ public class Client
 {
     public static void main(String args[])
     {
+        new Client();
+    }
+
+
+    public Client()
+    {
         try
         {
-            Socket clientSocket = new Socket("localhost", 5000);
+            //Establish connection to server
+            Socket clientSocket = new Socket("localhost", 6000);
 
-            Scanner keyboard = new Scanner(System.in);
+            if(clientSocket.isConnected())
+                System.out.println("Connected to server...\n");
+            else
+                System.out.println("Connection failed...\n");
 
-            String str = keyboard.nextLine();
+            //Wrap the IO streams
+            PrintWriter outputToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader inputFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            PrintWriter clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
-            clientOutput.println(str);
+            while(true)
+            {
+                //Collect input from client
+                Scanner keyboard = new Scanner(System.in);
+                System.out.print("Please enter a string: ");
+                String str = keyboard.nextLine();
 
-            BufferedReader clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("Client RECEIVED : "+clientInput.readLine()+"\n");
+                //Send client data to server
+                outputToServer.println(str);
+
+                //Receive and print data from server
+                System.out.println("\nClient RECEIVED : " + inputFromServer.readLine() + "\n\n");
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
