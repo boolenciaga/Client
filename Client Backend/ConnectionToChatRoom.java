286989f6be6fe661949bd15815056c5b447553bc
majinmilad <49212431+majinmilad@users.noreply.github.com>
2020-03-22 03:24:14 -0700

    implement ClientManager to handle multiple chat rooms
    Large update to the architecture of the server-client design for chat room
    
    ClientManagers works in conjuction with ServerManager to request and get connected to chat rooms
    
    ability to host multiple rooms is now live. Clients can make or join rooms by requesting a room name
    
    Note: this is a console app only at this point

---
+++ Client Backend/ConnectionToChatRoom.java
@@ -0,0 +1,114 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ConnectionToChatRoom implements Runnable
+{
+    //DATA MEMBERS
+    Socket socketToChatRoom;
+    private final int chatRoomPort;
+    private final String chatRoomName;
+    private final String myUserName;
+
+    private Thread mainThread;
+    private Thread listeningThread;
+
+    ObjectOutputStream objectOutputToServer;
+    ObjectInputStream objectInputFromServer;
+
+    Scanner keyboard = new Scanner(System.in);
+
+    //METHODS
+    public ConnectionToChatRoom(int portOfTheChatRoom, String nameOfTheChatRoom, String userName)
+    {
+        chatRoomPort = portOfTheChatRoom;
+        chatRoomName = nameOfTheChatRoom;
+        myUserName = userName;
+
+        //start this object on its own thread
+        mainThread = new Thread(this);
+        mainThread.start();
+    }
+
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            //Establish connection to server
+            socketToChatRoom = new Socket("localhost", chatRoomPort);
+
+            if(socketToChatRoom.isConnected())
+                System.out.println(myUserName + " is now connected to \"" + chatRoomName + "\"...\n");
+            else
+                System.out.println(myUserName + "'s connection failed...\n");
+
+            //Wrap the IO streams
+            wrapSocketStreams();
+
+            //output user name to chat room
+            objectOutputToServer.writeUTF(myUserName);
+            objectOutputToServer.flush();
+
+            //Start a thread for listening to incoming messages
+            listeningThread = new Thread(new listeningClass());
+            listeningThread.start();
+
+            System.out.println("BEGIN CHATTING...");
+
+            //Begin message relaying
+            while(true)
+            {
+                //collect chat input from client
+                String str = keyboard.nextLine();
+
+                //wrap input in a message
+                ChatMsg chatMsg = new ChatMsg(str, "The Chat Room", myUserName);
+
+                //send client message to chat room server
+                objectOutputToServer.writeObject(chatMsg);
+                objectOutputToServer.flush();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("exception caught in the run() of " + myUserName + "'s ConnectionToChatRoom\n");
+            e.printStackTrace();
+        }
+    }
+
+
+    private class listeningClass implements Runnable
+    {
+        ChatMsg incomingMsg;
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                //Continuously receive and print data from server
+                while(true)
+                {
+                    incomingMsg = (ChatMsg) objectInputFromServer.readObject();
+                    System.out.printf("%30s: ", incomingMsg.sentBy);
+                    System.out.println(incomingMsg.txt);
+                }
+            }
+            catch (IOException | ClassNotFoundException e)
+            {
+                System.out.println("exception caught in the run() of " + myUserName + "'s ConnectionToChatRoom.listeningClass\n");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void wrapSocketStreams() throws IOException
+    {
+        objectOutputToServer = new ObjectOutputStream(socketToChatRoom.getOutputStream());
+        objectInputFromServer = new ObjectInputStream(socketToChatRoom.getInputStream());
+    }
+}
