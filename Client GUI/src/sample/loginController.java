d640a18cbf85c45e2d99949bd23458e0876a5916
Kenneth Casimiro <36016735+boolenciaga@users.noreply.github.com>
2020-03-22 20:37:24 -0700

    User input selection

--- Client GUI/src/sample/loginController.java
+++ Client GUI/src/sample/loginController.java
@@ -3,13 +3,36 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+public class loginController {
 
-public abstract class loginController {
+    @FXML
+    private Label userNameLabel;
+
+    @FXML
+    private Button nextButton;
+
     @FXML
     private TextField usernameInput;
 
     @FXML
-    private Button nextButtonLogin;
+    void nextButtonClicked(ActionEvent event) {
+            if(!usernameInput.getText().isEmpty()) {
+
+                // Input Milad's Code
+
+                // If username input is successful -> go to next Window
+
+
+            }
+            else {
+                // Duplicate username? or username input is empty
+                userNameLabel.setText("INVALID INPUT - PLEASE TRY AGAIN");
+                usernameInput.clear();
+            }
+    }
+
 }
+
